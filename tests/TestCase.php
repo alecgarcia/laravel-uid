03a3f1bb9ce69eb01583bf074f9f8e3d7119c6be
Alec Garcia <alec@hyrograsper.com>
2022-01-23 17:20:09 -0600

    ignore php unit cache

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,10 +6,10 @@ use Carbon\Carbon;
 use Illuminate\Database\Capsule\Manager as DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Schema\Blueprint;
-use PHPUnit\Framework\TestCase as Base;
+use Orchestra\Testbench\TestCase;
 use Tests\Models\User;
 
-abstract class TestCase extends Base
+abstract class TestBase extends TestCase
 {
     protected function setUp(): void
     {
