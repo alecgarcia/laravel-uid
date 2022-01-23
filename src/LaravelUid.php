7182d267d3f47544df0aa9b1629d8c5f5d6526f3
Alec Garcia <alec@hyrograsper.com>
2022-01-22 19:40:15 -0600

    Add basic uid functionalitly.

--- src/LaravelUid.php
+++ src/LaravelUid.php
@@ -4,5 +4,32 @@ namespace Alecgarcia\LaravelUid;
 
 class LaravelUid
 {
-    // Build wonderful things
+    const ALPHA_NUM = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    // LarvelUid::make
+    // LaravelUid::check
+
+    /**
+     * @param  string|null  $prefix
+     * @param  int  $length
+     *
+     * @return string
+     */
+    public static function make(string $prefix = null, int $length = 16) : string
+    {
+        $uid = '';
+
+        if ($prefix && strlen($prefix) > 0) {
+            $uid = $prefix . '_';
+        }
+
+        $prefixLength = strlen($uid);
+
+        for ($i = 0; $i < ($length - $prefixLength); $i++) {
+            $index = mt_rand() % strlen(self::ALPHA_NUM);
+            $uid .= substr(self::ALPHA_NUM, $index, 1);
+        }
+
+        return $uid;
+    }
 }
---
+++ tests/LaravelUidTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests;
+
+use Tests\TestCase;
+use Alecgarcia\LaravelUid\LaravelUid;
+
+class LaravelUidTest extends TestCase
+{
+    /** @test */
+    public function can_generate_uid()
+    {
+        $uid = LaravelUid::make('prefix', 16);
+        $this->assertNotNull($uid);
+        $this->assertIsString($uid);
+    }
+
+    /** @test */
+    public function uid_length_changes()
+    {
+        $uid = LaravelUid::make('prefix', 10);
+        $this->assertEquals(10, strlen($uid));
+
+        $uid = LaravelUid::make('prefix', 24);
+        $this->assertEquals(24, strlen($uid));
+    }
+
+    /** @test */
+    public function uid_can_have_a_prefix()
+    {
+        $uid = LaravelUid::make('prefix', 17);
+        $this->assertStringContainsString('prefix_', $uid);
+    }
+
+    /** @test */
+    public function uid_does_not_need_a_prefix()
+    {
+        $uid = LaravelUid::make('', 17);
+        $this->assertEquals(17, strlen($uid));
+    }
+}
